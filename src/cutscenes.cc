f6b0ad41edaef82ef9304e25c91657fd9a0e147e
Parik <pariks@protonmail.com>
2021-08-27 20:12:39 +0530

    Cutscene Randomizer: Full cutscene randomizer
    * Randomizes the entire cutscene :thumbsup:

--- src/cutscenes.cc
+++ src/cutscenes.cc
@@ -7,6 +7,8 @@
 #include "CPed.hh"
 #include "CDrawCommands.hh"
 #include "CModelInfoStore.hh"
+#include <CTheScripts.hh>
+#include <array>
 
 int (*scanf__8665d0) (char *str, char *format, ...);
 void (__thiscall *DrawCutscenePed_0b99b41)(CPed*, void*, void*);
@@ -88,6 +90,44 @@ class CutsceneRandomizer
             ->Render ();
     }
 
+    /*******************************************************/
+    static void
+    RandomizeFullCutscene (NativeData *data)
+    {
+        constexpr static std::array sCutscenes
+            = {"B_1",     "B_2",     "B_5",     "B_6",     "BC_1",   "BC_2",
+               "BC_3",    "IMBC2",   "BR_1",    "BR_2",    "BR_3",   "BR_4",
+               "SHOW_1",  "SHOW_2",  "SHOW_3",  "SHOW_4",  "SHOW_5", "SHOW_6",
+               "SHOW_7",  "SHOW_8",  "N1_A1",   "N1_A2",   "N2",     "N3",
+               "N1",      "CM1",     "CM2",     "CM3",     "CM5",    "CM6",
+               "CM7",     "DM_1",    "DM_3",    "DF_2",    "DF_3",   "EM_1",
+               "EM_2",    "EM_5",    "EM_6",    "EM_7",    "EM_4",   "IMEM7",
+               "IMEM6",   "IMEM1",   "EM_4B",   "EM_3",    "FAU1_B", "FAU3_A1",
+               "FAU3_A2", "IMFAU6",  "FAU5_A",  "FAU1_A",  "FAU2_A", "FAU4_A",
+               "FAU6_A",  "FAU7_A",  "IMFAU2",  "IMFAU4",  "FAU3_A", "FIN_1",
+               "FIN_1AA", "FIN_1AB", "FIN_1BA", "FIN_1BB", "FM_1",   "FM_2",
+               "FM_3",    "FM_4",    "FM_5",    "FM6_A",   "FM6_B",  "G_1",
+               "G_2",     "G_3",     "GM_3F",   "GM_3D",   "GM_4",   "GM_5",
+               "IMGER3",  "IMGER4B", "IMGER4A", "GM_1",    "GM_2",   "IMGER4",
+               "IMGM5",   "INTROM",  "J_1",     "J_2",     "J_3",    "J_4",
+               "LILJ1_A", "LILJ2_A", "MICH_A",  "IMBHST",  "IMPM1",  "PM_1",
+               "PM_3",    "PM_2",    "IMPX4",   "PX_1",    "PX_2",   "PXDF",
+               "PX_1_A2", "PX_1_A1", "RPMAR1",  "RPBAD1",  "RPBRI1", "RPBRI2",
+               "RPBRI3",  "RPED1",   "RPED2",   "RPEXG1",  "RPGHN1", "RPHO1",
+               "RPILY1",  "RPIVN1",  "RPJEF1",  "RPJEF2",  "RPJEF3", "RPMAR2",
+               "RPMEL1",  "RPMOBD1", "RPPAT1",  "RPPAT2",  "RPSAR1", "RPSAR2",
+               "IMRAY1",  "IMRAY2",  "RB_1",    "RB_2",    "RB_3",   "RB_4",
+               "RB_4B",   "RB_5",    "RB_6",    "IMROM4",  "R_9",    "ROM1_B",
+               "ROM2_A",  "ROM3_A",  "ROM5_A",  "ROM6_A",  "ROM8_A", "ROM8_B",
+               "ROM8_C",  "RP_11",   "RP_11B",  "RP_12",   "RP_13",  "RP_14",
+               "WEDINT",  "VLA1_A",  "VLA2_A",  "VLA3_A",  "VLA4_A"};
+
+        data->Params[0].cstr_param
+            = (char *) sCutscenes[RandomInt (sCutscenes.size () - 1)];
+
+        CNativeManager::CallOriginalNative ("START_CUTSCENE_NOW", data);
+    }
+
 public:
     /*******************************************************/
     CutsceneRandomizer ()
@@ -108,6 +148,8 @@ public:
                        "c2 10 00 ff 74 ? ? ff 74 ? ? e8 ? ? ? ? ff 74"),
             ByVersion (15, 11), DrawCutscenePed_0b99b41, FixSuperLodFings);
 
+        CNativeManager::OverwriteNative("START_CUTSCENE_NOW", RandomizeFullCutscene);
+        
         Rainbomizer::Logger::LogMessage ("Initialised CutsceneRandomizer");
     }
 };
